35bc2e9198c47b9fe5f6f455d4f908c3e8878d7e
Tin Pavlinic <Tin.Pavlinic@team.telstra.com>
2019-08-02 14:33:27 +1000

    Add ttf library, be more principled about init and cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
-
-import SDL    
+import qualified SDL
+import qualified SDL.Font as SDLFont
+import qualified SDL.Time as SDLTime
 import Lib
-import SDL.Time (ticks)
 import Control.Monad (unless)
 import Control.Lens
 import Data.Maybe (catMaybes)
@@ -12,44 +12,52 @@ import Data.Maybe (catMaybes)
 
 main :: IO ()
 main = do
-    initializeAll
-    window <- createWindow "My SDL Application" defaultWindow
-    renderer <- createRenderer window (-1) defaultRenderer
-    initialTick <- ticks
+    initLibs
+    window <- SDL.createWindow "My SDL Application"  SDL.defaultWindow
+    renderer <-  SDL.createRenderer window (-1)  SDL.defaultRenderer
+    initialTick <- SDLTime.ticks
     appLoop (initialState initialTick) renderer
   
-appLoop :: State -> Renderer -> IO ()
+initLibs :: IO ()
+initLibs = do
+  SDL.initializeAll
+  SDLFont.initialize
+
+shutdown :: IO ()
+shutdown = do
+  SDLFont.quit
+  SDL.quit
+
+appLoop :: State -> SDL.Renderer -> IO ()
 appLoop state renderer = do
-    events <- pollEvents
-    tick <- ticks
+    events <-  SDL.pollEvents
+    tick <- SDLTime.ticks
     let qPressed = any eventIsQPress events
         updates = concat [
           if tick - view lastTick state > 1000 then [Tick tick] else [],
-          catMaybes (map (action . eventPayload) events)
+          catMaybes (map (action .  SDL.eventPayload) events)
           ]
         newState = foldl (\s u -> update u s) state updates
-    -- putStrLn $ "Events: " ++ show updates    
-    -- putStrLn $ "New State: " ++ show (view active newState) 
     renderState newState renderer
     unless qPressed (appLoop newState renderer)
    
-eventIsQPress :: Event -> Bool   
+eventIsQPress :: SDL.Event -> Bool   
 eventIsQPress event =
-  case eventPayload event of
-    KeyboardEvent keyboardEvent ->
-      keyboardEventKeyMotion keyboardEvent == Pressed &&
-      keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+  case  SDL.eventPayload event of
+    SDL.KeyboardEvent keyboardEvent ->
+      SDL.keyboardEventKeyMotion keyboardEvent ==  SDL.Pressed &&
+      SDL.keysymKeycode (SDL.keyboardEventKeysym keyboardEvent) ==  SDL.KeycodeQ
     _ -> False
 
-action :: EventPayload -> Maybe Update
-action (KeyboardEvent keyboardEvent)
-  | keyboardEventKeyMotion keyboardEvent == Pressed = extractEvent (keysymKeycode (keyboardEventKeysym keyboardEvent))
+action :: SDL.EventPayload -> Maybe Update
+action (SDL.KeyboardEvent keyboardEvent)
+  |  SDL.keyboardEventKeyMotion keyboardEvent ==  SDL.Pressed = extractEvent ( SDL.keysymKeycode ( SDL.keyboardEventKeysym keyboardEvent))
   | otherwise = Nothing
   where
-    extractEvent KeycodeLeft  = Just MoveLeft
-    extractEvent KeycodeRight = Just MoveRight
-    extractEvent KeycodeUp    = Just Rotate
-    extractEvent KeycodeDown  = Just Drop
+    extractEvent SDL.KeycodeLeft  = Just MoveLeft
+    extractEvent SDL.KeycodeRight = Just MoveRight
+    extractEvent SDL.KeycodeUp    = Just Rotate
+    extractEvent SDL.KeycodeDown  = Just Drop
     extractEvent _            = Nothing
 
 action _ = Nothing    
